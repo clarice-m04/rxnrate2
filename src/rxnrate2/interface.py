d74b7e05cf301dc2c696087633a327aea12718cf
clarice-m04 <mihalacheclarice@gmail.com>
2025-05-02 11:30:10 +0200

    env

--- src/rxnrate2/interface.py
+++ src/rxnrate2/interface.py
@@ -19,3 +19,10 @@ type_rxn1 = st.selectbox("Select the type of reaction: ", options)
 
 st.write("You have chosen the mode : Reaction of type ")
 
+from rxnrate2.singlerxn1 import calculate_laplace_transforms, inverse_laplace_transform
+reagents = ['water', 'potassium']
+products = ['hydrogen peroxide', 'oxygen']
+k = 2
+initial_concentrations = {'water': 1, 'potassium': 2, 'hydrogen peroxide': 0, 'oxygen': 0}
+
+laplace_res = calculate_laplace_transforms(reagents, products, k, initial_concentrations)
