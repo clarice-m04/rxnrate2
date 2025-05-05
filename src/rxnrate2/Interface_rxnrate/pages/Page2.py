2286a82830d9393788846cca0da011357cae5f07
Elo1402 <elodie-angelique.lendi@epfl.ch>
2025-05-05 22:32:22 +0200

    Change for Home Page

--- src/rxnrate2/Interface_rxnrate/pages/Page2.py
+++
@@ -1,8 +0,0 @@
-from LaplaceTransform_singlerxn import calculate_laplace_transforms 
-
-reagents = ['A', 'B', 'C', 'F']
-products = ['D', 'E']
-k = 2
-initial_concentrations = {'A': 1, 'B': 2, 'C': 1, 'D': 0, 'E': 0, 'F': 1.0}
-
-laplace_res = calculate_laplace_transforms(reagents, products, k, initial_concentrations)
---
+++ src/rxnrate2/Interface_rxnrate/pages/SimpleRxn.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# Times new roman font
+st.markdown("""
+    <style>
+    * {
+        font-family: 'Times New Roman', Times, serif !important;
+    }
+    html, body, [class*="css"] {
+        font-family: 'Times New Roman', Times, serif !important;
+    }
+    .stText, .stMarkdown, .stDataFrame, .stTable, .stButton, .stHeader, .stSubheader, .stCaption {
+        font-family: 'Times New Roman', Times, serif !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+def linear_rxn():
+    st.title("Welcome in linear reaction part")
