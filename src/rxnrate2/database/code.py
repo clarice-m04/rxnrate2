03712eaf678139f35aa5a3edefb2f103e52fa240
AmbrineL <ambrine.labout@epfl.ch>
2025-05-13 16:05:03 +0200

    Changement des lignes d'importation

--- src/rxnrate2/database/code.py
+++ src/rxnrate2/database/code.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Draw
-from rdkit.Chem.Draw import IPythonConsole
-IPythonConsole.ipython_useSVG=True
 
 data = pd.read_csv('Data_projet.csv', sep = ";")
 df = pd.DataFrame(data)
